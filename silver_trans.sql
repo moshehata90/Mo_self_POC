6dc3a9aa4727b96c24c2a99902fa6301e90c58b9
Mohamed Shehata <mohamed.shehata@etraveligroup.com>
2024-04-30 11:15:12 +0200

    new silver transformation modificationagain 101

--- silver_trans.sql
+++ silver_trans.sql
@@ -1,5 +1,4 @@
-with mydataset ( select * from `ibe`.`cart_order_business_events`)
-SELECT * FROM mydataset  WHERE make_order_process_id is NOT null ;
+SELECT * FROM `ibe`.`cart_order_business_events`  WHERE make_order_process_id is NOT null ;
 
 
 
